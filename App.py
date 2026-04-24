e868f59eae968ec5fd25a066845afba2035690d3
Jefferson Riann Meira Santos <235415849+DevRiann@users.noreply.github.com>
2026-04-23 22:45:22 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 from streamlit_gsheets import GSheetsConnection
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
